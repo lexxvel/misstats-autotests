2218ec9e3cbe9d99d096a028c8a5808c216e3597
Aleksandr <lexxvel@outlook.com>
2022-07-20 00:48:43 +0500

    добавил файлы в гит

---
+++ src/main/java/pageobjects/Header.java
@@ -0,0 +1,9 @@
+package pageobjects;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Header {
+    private static final SelenideElement header = $x("");
+}
